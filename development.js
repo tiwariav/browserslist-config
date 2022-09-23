55f778238cab510bc469cf8070290269d76fee63
Arvind Tiwari <tiwariav@gmail.com>
2022-09-23 05:37:50 +0530

    initial commit

---
+++ development.js
@@ -0,0 +1,6 @@
+/* eslint-disable unicorn/prefer-module */
+module.exports = [
+  "last 1 chrome version",
+  "last 1 firefox version",
+  "last 1 safari version",
+];
---
+++ production.js
@@ -0,0 +1,2 @@
+/* eslint-disable unicorn/prefer-module */
+module.exports = [">0.2%", "not dead", "not IE 11", "not op_mini all"];
